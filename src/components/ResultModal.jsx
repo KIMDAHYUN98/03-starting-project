9980faa41f4b9c5e91321c334373835866607bdd
kdh98 <dbkdh98@gmail.com>
2025-12-26 11:44:49 +0900

    ✨ Section 8 : 143. 데모 앱의 "결과 모달창" 개선

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -4,26 +4,33 @@ import { forwardRef, useImperativeHandle, useRef } from 'react';
 // 컴포넌트 함수 내에 호출하여 컴포넌트 외부에서 접근할 수 있는 프로퍼티와 메서드를 정의
 // 자주 사용 X, 프로퍼티나 다른 방식 선호
 
-const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset }, ref) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2); // 소수점 두 자리수까지만 표시
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
   useImperativeHandle(ref, () => {
     return {
       open() {
         dialog.current.showModal();
-      }
-    }
+      },
+    };
   });
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+      {userLost && <h2>You lost</h2>}
+      {!userLost && <h2>You Score: {score}</h2>}
       <p>
         목표 시간은 <strong>{targetTime}초였습니다.</strong>
       </p>
       <p>
-        타이머를 <strong>X초가 남은 상태에서</strong> 멈췄습니다.
+        타이머를 <strong>{formattedRemainingTime} 초가 남은 상태에서</strong> 멈췄습니다.
       </p>
-      <form action="dialog">
+      <form action="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,42 +1,55 @@
 import { useState, useRef } from "react";
 import ResultModal from "./ResultModal";
 
-//let timer;
-
 export default function TimeChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
 
-  const [timerStarted, setTimerStarted] = useState(false);
-  const [timerExpired, setTimeExpired] = useState(false);
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000)
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if(timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
+
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
+  };
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setTimeExpired(true);
-      dialog.current.open(); // ResultModal 컴포넌트에서 정의한 함수
-    }, targetTime * 1000);
-    setTimerStarted(true);
+    timer.current = setInterval(() => {
+      setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+    }, 10);
   };
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   };
 
   return ( 
     <>
-      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
+      <ResultModal 
+        ref={dialog} 
+        targetTime={targetTime} 
+        result="lost" 
+        remainingTime={timeRemaining}
+        onReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? 's' : ''}
         </p>
         <p>
-          <button onClick={timerStarted ? handleStop : handleStart}>
-            {timerStarted ? 'Stop' : 'Start'} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? 'Stop' : 'Start'} Challenge
           </button>
         </p>
-        <p className={timerStarted ? 'active' : undefined}>
-          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        <p className={timerIsActive ? 'active' : undefined}>
+          {timerIsActive ? '시간이 흐르고 있어요...' : '타이머가 비활성 상태입니다'}
         </p>
       </section>
     </>
