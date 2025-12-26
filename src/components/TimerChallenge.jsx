a5fb5c98aaa085ac4498a52cca3094dea6d9acc8
kdh98 <dbkdh98@gmail.com>
2025-12-26 09:23:20 +0900

    ✨ Section 8 : 137. "DOM 연결" 외 Refs(참조) 활용법

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+//let timer;
 
 export default function TimeChallenge({ title, targetTime }) {
-  const [timerExpired, setTimeExpired] = useState(false);
+  const timer = useRef();
+
   const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimeExpired] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimeExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
   };
 
   function handleStop() {
-    
+    clearTimeout(timer.current);
   };
 
   return ( 
@@ -23,7 +27,7 @@ export default function TimeChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? 'Stop' : 'Start'} Challenge
         </button>
       </p>
